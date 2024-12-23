620a7164d9386519f7895aa8d4839498bba713e8
JohnPWright <72052019+JohnPWright@users.noreply.github.com>
2024-12-22 19:54:20 -0800

    Created a Header Component
    created a Header component, i also created a Pages folder to hold all the pages for the project, and created a PortfolioPage file instead of putting it all in App.tsx

---
+++ src/pages/PortfolioPage/PortfolioPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+
+const PortfolioPage = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-800">
+      <Header title="Portfolio Page" />
+    </div>
+  )
+}
+
+export default PortfolioPage

8d29e50037612dde5340eef8a76cb207f10d894d
JohnPWright <72052019+JohnPWright@users.noreply.github.com>
2024-12-22 23:51:54 -0800

    Created tagline Prop

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,14 +2,18 @@ import React from 'react'
 
 type HeaderProps = {
   title: string;
+  tagline: string;
 };
 
-const Header = ({ title }: HeaderProps) => {
+const Header = ({ title, tagline }: HeaderProps) => {
   return (
     <div>
       <h1 className="text-5xl font-semibold tracking-tight text-white sm:text-7xl">
         {title}
       </h1>
+      <div className="hidden md:block text-gray-400">
+        {tagline}
+      </div>
     </div>
   )
 }
--- src/pages/PortfolioPage/PortfolioPage.tsx
+++ src/pages/PortfolioPage/PortfolioPage.tsx
@@ -4,7 +4,10 @@ import Header from '../../components/Header/Header'
 const PortfolioPage = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-800">
-      <Header title="Portfolio Page" />
+      <Header
+        title="John Wright"
+        tagline="Solar Service Technician | Aspiring Software Developer"
+      />
     </div>
   )
 }
